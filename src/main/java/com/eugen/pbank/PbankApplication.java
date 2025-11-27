d1091b7bc68ea606b17410e8cc7a2508a9bc9a23
eugen_kowal <eugenkowal88@gmail.com>
2025-11-27 11:41:33 +0100

    connected to DB

---
+++ src/main/java/com/eugen/pbank/PbankApplication.java
@@ -0,0 +1,13 @@
+package com.eugen.pbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eugen/pbank/PbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugen.pbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
